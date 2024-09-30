cec7c1b4ad97a52c5bea9964180da6bb60a64462
cesarr340 <cesarr340@gmail.com>
2024-09-30 12:13:21 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import logo from './assets/logo-astroatlas.svg'
+
+export function App() {
+  return(
+    <>
+      <img src={logo} style={{ width: '150px', height: '150px', color: 'red' }} />
+    </>
+  )
+}
+
+export default App
