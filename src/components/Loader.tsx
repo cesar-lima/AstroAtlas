94f5336dddaf30263c2d23760ea04da242c59f3c
cesarr340 <cesarr340@gmail.com>
2024-10-01 12:19:13 +0000

    adicionar componente loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,33 @@
+import '../styles/loader.scss'
+
+export function Loader() {
+    return (
+        <section className="loader-container">
+            <div className="loader">
+                <div className="box">
+                    <div className="logo">
+                        <svg version="1.1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            // xmlns:xlink="http://www.w3.org/1999/xlink" 
+                            className="svg"
+                            viewBox="0 0 200 200"
+                        >
+                            <g data-paper-data="{&quot;fillRule&quot;:&quot;nonzero&quot;,&quot;isIcon&quot;:&quot;true&quot;,&quot;iconType&quot;:&quot;icon&quot;,&quot;rawIconId&quot;:&quot;a838a5e4-3855-43eb-a966-46832627a1a2&quot;,&quot;selectedEffects&quot;:{&quot;container&quot;:&quot;circle_simple_filled&quot;,&quot;transformation&quot;:&quot;&quot;,&quot;pattern&quot;:&quot;&quot;},&quot;isDetailed&quot;:false,&quot;blockLetter&quot;:&quot;O&quot;,&quot;blockLetterId&quot;:0,&quot;relativeBoundsIconToContainer&quot;:{&quot;top&quot;:-0.028646275573654532,&quot;left&quot;:-5.2644157490980135e-9},&quot;widthRatioIconToContainer&quot;:0.8938661844954512,&quot;heightRatioIconToContainer&quot;:0.8280568774127238,&quot;bounds&quot;:{&quot;x&quot;:220.03028169014084,&quot;y&quot;:150.54228873239435,&quot;width&quot;:109.93943661971832,&quot;height&quot;:109.93943661971832},&quot;iconStyle&quot;:&quot;standalone&quot;}">
+                                <path
+                                    d="M0,100c0,-55.22847 44.77153,-100 100,-100c55.22847,0 100,44.77153 100,100c0,55.22847 -44.77153,100 -100,100c-55.22847,0 -100,-44.77153 -100,-100zM92.22726,68.67249l-39.79648,86.43297c-16.47823,-14.30186 -24.71734,-32.38645 -24.71734,-54.25379c0,-19.89824 7.04729,-36.94647 21.14187,-51.14469c14.19822,-14.09458 31.24645,-21.14187 51.14469,-21.14187c19.89824,0 36.89465,7.04729 50.98924,21.14187c14.09458,14.19822 21.14187,31.24645 21.14187,51.14469c0,21.86734 -8.23911,39.95193 -24.71734,54.25379l-39.64103,-86.43297c-1.55455,-3.31638 -4.14547,-4.97456 -7.77275,-4.97456c-3.73092,0 -6.32183,1.65819 -7.77275,4.97456zM148.81287,175.62552c12.64367,-8.29093 22.54097,-19.06914 29.6919,-32.33463c7.25456,-13.16186 10.88185,-27.30826 10.88185,-42.43921c0,-24.66553 -8.7573,-45.70377 -26.27189,-63.11473c-17.41097,-17.51459 -38.44921,-26.27189 -63.11473,-26.27189c-24.66553,0 -45.70377,8.7573 -63.11473,26.27189c-17.51459,17.41097 -26.27189,38.44921 -26.27189,63.11473c0,15.13096 3.57546,29.27735 10.72639,42.43921c7.15093,13.26549 17.10004,24.0437 29.84735,32.33463c2.17638,1.45091 4.5082,1.81364 6.99548,1.08818c2.48728,-0.72545 4.30093,-2.28 5.44093,-4.66365l36.37647,-79.28204l36.37647,79.28204c1.03637,2.38365 2.85001,3.9382 5.44093,4.66365c0.72546,0.20728 1.50273,0.31091 2.33183,0.31091c1.65818,0 3.21273,-0.46637 4.66365,-1.3991z"
+                                    fillRule="nonzero"
+                                    fill="#ffffff"
+                                >
+                                </path>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+            </div>
+        </section>
+    )
+}
