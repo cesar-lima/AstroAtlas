71cc054d617f538dd02c9826780cebcc2d88ad51
cesarr340 <cesarr340@gmail.com>
2024-10-01 12:20:02 +0000

    criar pagina planets e seus respectivos estilos e rota

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './styles/global.scss'
-import { Getstarted } from './pages/getstarted.tsx'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Getstarted } from './pages/getstarted.tsx'
+import { Planets } from './pages/planets.tsx';
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Getstarted />} />
+        <Route path="/planets" element={<Planets />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/planets.tsx
@@ -0,0 +1,9 @@
+import '../styles/planets.scss'
+
+export function Planets() {
+    return(
+        <div>
+            <h1>Planets</h1>
+        </div>
+    )
+}
