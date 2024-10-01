3047d717a20002425ee5e6f0ba6075a89a263e34
cesarr340 <cesarr340@gmail.com>
2024-10-01 00:51:25 +0000

    finalizar pagina inicial e adicionar fontes e lib para icones

--- src/pages/getstarted.tsx
+++ src/pages/getstarted.tsx
@@ -1,7 +1,40 @@
+import '../styles/getstarted.scss'
+
+import logo from '../assets/logo-astroatlas.svg'
+import { ArrowSquareOut } from '@phosphor-icons/react'
+import { Link } from 'react-router-dom'
+
 export function Getstarted() {
-    return(
-        <div>
-            hello world
+    return (
+        <div className="container-getstarted">
+            <nav>
+                <div className="logo">
+                    <img src={logo} alt="logo astro atlas" />
+                </div>
+
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </nav>
+
+            <header>
+                <p>
+                    Let's Explore the <span>Universe!&nbsp;</span>
+                </p>
+
+                <p>
+                    Unveil the secrets of the planets and explore the universe.
+                </p>
+            </header>
+
+            <footer>
+                <Link to="/planets">
+                    <button>
+                        <div>Get Started</div>
+                        <ArrowSquareOut size={24}  />
+                    </button>
+                </Link>
+            </footer>
         </div>
     )
 }
