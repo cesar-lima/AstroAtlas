fe930d9a3f2ead12ad7730522a37765a24679eab
cesarr340 <cesarr340@gmail.com>
2024-10-04 00:03:17 +0000

    iniciar construção da página de planetas

--- src/pages/planets.tsx
+++ src/pages/planets.tsx
@@ -1,9 +1,45 @@
 import '../styles/planets.scss'
 
+import { useEffect, useState } from 'react';
+
+import { Loader } from '../components/Loader';
+import { Previous } from '../components/previous-page-btn';
+import mars from '../assets/mars.png'
+
 export function Planets() {
-    return(
-        <div>
-            <h1>Planets</h1>
-        </div>
-    )
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {loading && <Loader />}
+
+      <div className="planets-container">
+        <Previous />
+
+        <section>
+          <header className="planet-name">
+            MARS
+          </header>
+
+          <article className="planet-infos">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto fuga quia repellendus reiciendis exercitationem ullam, eveniet tempore iusto obcaecati dicta, nostrum voluptatum! Repudiandae inventore nihil, vero voluptas quod nobis at.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto fuga quia repellendus reiciendis exercitationem ullam, eveniet tempore iusto obcaecati dicta, nostrum voluptatum! Repudiandae inventore nihil, vero voluptas quod nobis at.
+          </article>
+
+          <div className="planet-image">
+            <img src={mars} alt="planet image" />
+          </div>
+
+        </section>
+      </div>
+    </>
+  )
 }
